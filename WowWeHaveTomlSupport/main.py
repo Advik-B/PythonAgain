56c9089396df0149e23ce411ac9b65a71af0b4dc
Advik <advik.b@gmail.com>
2022-10-29 00:03:28 +0530

    Update main.py

--- WowWeHaveTomlSupport/main.py
+++ WowWeHaveTomlSupport/main.py
@@ -1,7 +1,22 @@
 import tomllib
+import sys
+
+sys.setrecursionlimit(10000)
 
 # With tomllib, you WILL HAVE TO USE Read-Binary mode
 with open("sometoml.toml", "rb") as f:
     toml = tomllib.load(f)
 
-print(toml)
+def print_kv(dictobj: dict, indent: int = 0):
+    for key, value in dictobj.items():
+        if isinstance(value, dict):
+            print(" " * indent, key+":")
+            try:
+                print_kv(value, indent + 4)
+            except RecursionError:
+                print(" " * (indent + 4), "...")
+        else:
+            print(" " * indent, key+":", value)
+
+
+print_kv(toml)
