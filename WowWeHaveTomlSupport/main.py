435fc54864f9f666737e2299d5c7de6a9e20598c
Advik <advik.b@gmail.com>
2022-10-29 00:14:40 +0530

    Update main.py

--- WowWeHaveTomlSupport/main.py
+++ WowWeHaveTomlSupport/main.py
@@ -1,8 +1,6 @@
 import tomllib
-import sys
 import json
-from typing import Union
-sys.setrecursionlimit(10000)
+import yaml
 
 # With tomllib, you WILL HAVE TO USE Read-Binary mode
 with open("sometoml.toml", "rb") as f:
@@ -11,22 +9,7 @@ with open("sometoml.toml", "rb") as f:
 with open("somejson.json", "r") as f:
     jayson = json.load(f)
 
-def print_kv(dataobj: Union[dict, list], indent: int = 0):
-    if isinstance(dataobj, dict):
-        for key, value in dataobj.items():
-            if isinstance(value, dict):
-                print(" " * indent, key+":")
-                try:
-                    print_kv(value, indent + 4)
-                except RecursionError:
-                    print(" " * (indent + 4), "...")
-            else:
-                print(" " * indent, key+":", value)
-    elif isinstance(dataobj, list):
-        for value in dataobj:
-            print()
-            print_kv(value, indent)
+with open("someyaml.yaml", "r") as f:
+    yam = yaml.load(f, Loader=yaml.SafeLoader)
 
 
-# print_kv(toml)
-print_kv(jayson)
